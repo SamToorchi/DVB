1999cb3c78dfb6ae24df14fff388c6767e898318
SamToorchi <stoorchi@googlemail.com>
2019-12-02 18:14:58 +0100

    nothing works again

---
+++ visualization/sample_axis.js
@@ -0,0 +1,98 @@
+    var sampleAxis = [
+        {
+            clock: '0',
+            diff_hour: '0'
+      },
+        {
+            clock: '1',
+            diff_hour: '0'
+      },
+        {
+            clock: '2',
+            diff_hour: '0'
+      },
+        {
+            clock: '3',
+            diff_hour: '0'
+      },
+        {
+            clock: '4',
+            diff_hour: '0'
+      },
+        {
+            clock: '5',
+            diff_hour: '0'
+      },
+        {
+            clock: '6',
+            diff_hour: '0'
+      },
+        {
+            clock: '7',
+            diff_hour: '0'
+      },
+        {
+            clock: '8',
+            diff_hour: '0'
+      },
+        {
+            clock: '9',
+            diff_hour: '0'
+      },
+        {
+            clock: '10',
+            diff_hour: '0'
+      },
+        {
+            clock: '11',
+            diff_hour: '0'
+      },
+        {
+            clock: '12',
+            diff_hour: '0'
+      },
+        {
+            clock: '13',
+            diff_hour: '0'
+      },
+        {
+            clock: '14',
+            diff_hour: '0'
+      },
+        {
+            clock: '15',
+            diff_hour: '0'
+      },
+        {
+            clock: '16',
+            diff_hour: '0'
+      },
+        {
+            clock: '17',
+            diff_hour: '0'
+      },
+        {
+            clock: '18',
+            diff_hour: '0'
+      },
+        {
+            clock: '19',
+            diff_hour: '0'
+      },
+        {
+            clock: '20',
+            diff_hour: '0'
+      },
+        {
+            clock: '21',
+            diff_hour: '0'
+      },
+        {
+            clock: '22',
+            diff_hour: '0'
+      },
+        {
+            clock: '23',
+            diff_hour: '0'
+      }
+        ]
